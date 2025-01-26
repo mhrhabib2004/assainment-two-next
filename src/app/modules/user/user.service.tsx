0c3e9c9ae026605e0c1555cbb0d0e77cd0589933
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-01-27 03:00:37 +0600

    up:user service

--- src/app/modules/user/user.service.tsx
+++ src/app/modules/user/user.service.tsx
@@ -1,87 +1,66 @@
-import { Model, Schema } from "mongoose";
+
+
+// const createAcademicDepartmentIntoDB = async (payload: TAcademicDepartment) => {
+//     const result = await AcademicDepartment.create(payload);
+//     return result;
+// };
+
 import { TUser } from "./user.model";
-import { config } from "dotenv";
 
-// Specific filter interface data
-interface UserModel extends Model<TUser> {
-    getAuthUserData(userId: string): Promise<TUser | null>;
-    getPublicUserData(userId: string): Promise<Pick<TUser, '_id' | 'name' | 'email'>>;
-}
+// User Creat Function
+const createUserIntoDB = async ( payload: TUser) => {
 
-// user Model
-const userSchema = new Schema<TUser>(
-    {
-        name: {
-            type: String,
-            required: [true, 'Name is required'],
-            maxlength: 20,
-            trim: true
-        },
-        email: {
-            type: String,
-            required: [true, 'Email is required'],
-            unique: true,
-            trim: true,
-            validate: {
-                validator: function (value: string) {
-                    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(value);
-                },
-                message: '{VALUE} is not a valid email',
-            },
-            immutable: true,
-        },
-        password: {
-            type: String,
-            required: [true, 'Password is Required'],
-            select: false
-        },
-        role: {
-            type: String,
-            enum: {
-                values: ['user', 'admin'],
-                message: '{VALUE} is not valid, please provide a valid role',
-            },
-            default: 'user',
-            required: true,
-        },
-        isBlocked: {
-            type: Boolean,
-            default: false,
-        },
-    },
-    {
-        timestamps: true,
-    },
-);
+    const result = await User.create(payload);
 
-// Password hashing function
-userSchema.pre('save', async function (next) {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const user = this;
-    if (user.isModified('password')) {
-        user.password = await bcrypt.hash(
-            user.password,
-            Number(config.bcrypt_salt_rounds),
-        );
-    }
-    next();
-});
+    
+    return result;
 
-// Clear password after saving
-userSchema.post('save', function (doc, next) {
-    doc.password = '';
-    next();
-});
+    // // create a user object
+    // const userData: Partial<TUser> = {};
 
-// Get specific user data for authentication
-userSchema.statics.getAuthUserData = function (userId: string) {
-    return this.findById(userId).select('+password _id name email role isBlocked');
+    // //if password is not given , use deafult password
+    // userData.password = password || (config.default_password as string);
+
+    // //set student role
+    // userData.role = 'admin';
+
+    // const session = await mongoose.startSession();
+
+    // try {
+    //     session.startTransaction();
+    //     //set  generated id
+    //     userData.id = await generateAdminId();
+
+    //     // create a user (transaction-1)
+    //     const newUser = await User.create([userData], { session });
+
+    //     //create a admin
+    //     if (!newUser.length) {
+    //         throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create admin');
+    //     }
+    //     // set id , _id as user
+    //     payload.id = newUser[0].id;
+    //     payload.user = newUser[0]._id; //reference _id
+
+    //     // create a admin (transaction-2)
+    //     const newAdmin = await Admin.create([payload], { session });
+
+    //     if (!newAdmin.length) {
+    //         throw new AppError(httpStatus.BAD_REQUEST, 'Failed to create admin');
+    //     }
+
+    //     await session.commitTransaction();
+    //     await session.endSession();
+
+    //     return newAdmin;
+    //     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    // } catch (err: any) {
+    //     await session.abortTransaction();
+    //     await session.endSession();
+    //     throw new Error(err);
+    // }
 };
 
-// Get public user data
-userSchema.statics.getPublicUserData = function (userId: string) {
-    return this.findById(userId).select('_id name email');
+export const UserServices = {
+    createUserIntoDB,
 };
-
-// Export User model
-export const User = model<TUser, UserModel>('User', userSchema);
