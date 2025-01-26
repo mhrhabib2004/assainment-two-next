0e048c09ed8b60a07ad5490ecd27952b08951dba
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-01-27 02:46:52 +0600

    up:user controler

---
+++ src/app/modules/user/user.controller.tsx
@@ -0,0 +1,17 @@
+/ User Create Funtionality
+const createUser = catchAsync(async (req, res) => {
+  const { user } = req.body;
+
+  const result = await UserServices.createUserIntoDB(user);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'User is created succesfully',
+    data: result,
+  });
+});
+
+export const UserControllers = {
+  createUser,
+};
