0db06d594833d069f315609180240cf6013e4f05
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-01-27 02:51:11 +0600

    up:userinterface

---
+++ src/app/modules/user/user.model.tsx
@@ -0,0 +1,18 @@
+
+// import { USER_ROLE } from "./user.constant";
+
+import { USER_ROLE } from "./user.consatnt";
+
+
+
+
+export type TUser = {
+    _id?: string;
+    name: string;
+    email: string;
+    password: string;
+    role: 'admin' | 'user';
+    isBlocked: boolean;
+};
+
+export type TUserRole = keyof typeof USER_ROLE ;
