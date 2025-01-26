4be0737ea9469bb8260a6b5d509b22422fd83929
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-01-27 02:43:24 +0600

    add:user

--- dist/route/product.route.js
+++ dist/route/product.route.js
@@ -4,7 +4,7 @@ const express_1 = require("express");
 const product_controllers_1 = require("../controllers/product.controllers");
 const productRouter = (0, express_1.Router)();
 // Endpoint to create a new product
-productRouter.post("/create-product", product_controllers_1.productController.createProduct);
+productRouter.post("/", product_controllers_1.productController.createProduct);
 // Endpoint to get all products
 productRouter.get("/", product_controllers_1.productController.getAllProducts);
 productRouter.get("/:productId", product_controllers_1.productController.getProductById);
--- src/route/product.route.ts
+++ src/route/product.route.ts
@@ -7,7 +7,7 @@ import {productController } from "../controllers/product.controllers";
 const productRouter = Router();
 
 // Endpoint to create a new product
-productRouter.post("/create-product", productController.createProduct);
+productRouter.post("/", productController.createProduct);
 
 // Endpoint to get all products
 productRouter.get("/", productController.getAllProducts);
