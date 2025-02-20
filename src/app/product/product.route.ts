0cc2d556c970628179337002b4811474b9ea6719
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-02-21 02:21:49 +0600

    up:product route

--- src/app/product/product.route.ts
+++ src/app/product/product.route.ts
@@ -1,17 +1,34 @@
 import { Router } from "express";
+import { ProductValidations } from "./product.validations"; // Import validation schemas
+import validateRequest from "../middlewares/validateRequest";
 import { productController } from "./product.controllers";
+import auth from "../middlewares/auth";
+import { USER_ROLE } from "../modules/user/user.consatnt";
 
 
 const productRouter = Router();
 
 // Endpoint to create a new product
-productRouter.post("/", productController.createProduct);
+productRouter.post(
+    "/create-product",auth(USER_ROLE.admin),
+    validateRequest(ProductValidations.createProductValidationSchema), 
+    productController.createProduct
+);
 
 // Endpoint to get all products
 productRouter.get("/", productController.getAllProducts);
 
+// Endpoint to get a single product by ID
 productRouter.get("/:productId", productController.getProductById);
-productRouter.put("/:productId", productController.updateProduct);
+
+// Endpoint to update a product by ID
+productRouter.put(
+    "/:productId",auth(USER_ROLE.admin),
+    validateRequest(ProductValidations.updateProductValidationSchema), 
+    productController.updateProduct
+);
+
+// Endpoint to delete a product by ID
 productRouter.delete("/:productId", productController.deleteProduct);
 
 export default productRouter;
