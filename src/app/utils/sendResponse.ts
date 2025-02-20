abd4efc7902b1a569927bd5b4adb26e95adf99f3
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-02-21 05:25:47 +0600

    up:send response

--- src/app/utils/sendResponse.ts
+++ src/app/utils/sendResponse.ts
@@ -1,8 +1,18 @@
-import { Response } from "express";
+
+import { Response } from 'express';
+
+type TMeta = {
+    limit: number;
+    page: number;
+    total: number;
+    totalPage: number;
+};
+
 type TResponse<T> = {
     statusCode: number;
     success: boolean;
     message?: string;
+    meta?: TMeta;
     data: T;
 };
 
@@ -10,7 +20,7 @@ const sendResponse = <T>(res: Response, data: TResponse<T>) => {
     res.status(data?.statusCode).json({
         success: data.success,
         message: data.message,
-        statusCode: data.statusCode,
+        meta: data.meta,
         data: data.data,
     });
 };
