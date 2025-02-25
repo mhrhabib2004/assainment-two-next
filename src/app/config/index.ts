5d0a3c72d03584cce29883398a2ce6cad41fe76b
mhrhabib2004 <mhrhabib2004@gmail.com>
2025-02-26 02:54:07 +0600

    up:index

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -8,5 +8,12 @@ export default {
     port: process.env.PORT,
     bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
     jwt_access_secret: process.env.JWT_ACCESS_SECRET,
-    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN
+    jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+    sp: {
+        sp_endpoint: process.env.SP_ENDPOINT, // ShurjoPay endpoint
+        sp_username: process.env.SP_USERNAME, // ShurjoPay username
+        sp_password: process.env.SP_PASSWORD, // ShurjoPay password
+        sp_prefix: process.env.SP_PREFIX, // ShurjoPay prefix
+        sp_return_url: process.env.SP_RETURN_URL, // ShurjoPay return URL
+    },
 }
